9ae13176369ed39ff17b72c0f77b549beb765d85
Pio <pio.cho.bia@gmail.com>
2022-02-26 10:53:04 +0100

    Dodaj pliki projektów.

---
+++ Kalkulator22_2/Kalkulator22_2/MainPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Kalkulator22_2
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonCE_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonMRC_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonMSubtraction_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonMAddition_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
