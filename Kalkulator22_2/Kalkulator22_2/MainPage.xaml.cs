a960ef1ef291c70ad0275f06c26a9d828ccdc284
Pio <pio.cho.bia@gmail.com>
2022-03-06 20:34:05 +0100

    klawisze cd 2

--- Kalkulator22_2/Kalkulator22_2/MainPage.xaml.cs
+++ Kalkulator22_2/Kalkulator22_2/MainPage.xaml.cs
@@ -15,24 +15,142 @@ namespace Kalkulator22_2
             InitializeComponent();
         }
 
-        private void ButtonCE_Clicked(object sender, EventArgs e)
-        {
 
-        }
 
         private void buttonMRC_Clicked(object sender, EventArgs e)
-        {
+        { 
 
         }
 
         private void buttonMSubtraction_Clicked(object sender, EventArgs e)
         {
-
+             
         }
 
         private void buttonMAddition_Clicked(object sender, EventArgs e)
         {
 
         }
+
+
+
+ 
+        private void button0_Clicked(object sender, EventArgs e)
+        {
+            labelDisplay0.Text += "0";
+        }
+
+        private void button1_Clicked(object sender, EventArgs e)
+        {
+            labelDisplay0.Text += "1";
+        }
+
+        private void button2_Clicked(object sender, EventArgs e)
+        {
+            labelDisplay0.Text += "2";
+        }
+
+        private void button3_Clicked(object sender, EventArgs e)
+        {
+            labelDisplay0.Text += "3";
+        }
+
+        private void button4_Clicked(object sender, EventArgs e)
+        {
+            labelDisplay0.Text += "4";
+        }
+
+        private void button5_Clicked(object sender, EventArgs e)
+        {
+            labelDisplay0.Text += "5";
+        }
+
+        private void button6_Clicked(object sender, EventArgs e)
+        {
+            labelDisplay0.Text += "6";
+        }
+
+        private void button7_Clicked(object sender, EventArgs e)
+        {
+            labelDisplay0.Text += "7";
+        }
+
+        private void button8_Clicked(object sender, EventArgs e)
+        {
+            labelDisplay0.Text += "8";
+        }
+
+        private void button9_Clicked(object sender, EventArgs e)
+        {
+            labelDisplay0.Text += "9";
+        }
+
+        private void buttonDot_Clicked(object sender, EventArgs e)
+        {
+            labelDisplay0.Text += ".";
+        }
+
+        private void buttonAddition_Clicked(object sender, EventArgs e)
+        {
+            labelDisplay0.Text += "+";
+        }
+
+        private void buttonSubtraction_Clicked(object sender, EventArgs e)
+        {
+            labelDisplay0.Text += "-";
+        }
+
+        private void buttonMultiplication_Clicked(object sender, EventArgs e)
+        {
+            labelDisplay0.Text += "*";
+        }
+
+        private void buttonDivide_Clicked(object sender, EventArgs e)
+        {
+            labelDisplay0.Text += "/";
+        }
+
+        private void ButtonCE_Clicked(object sender, EventArgs e)
+        {
+            labelDisplay0.Text = "";
+           
+        }
+
+        private void buttonDeleteSign_Clicked(object sender, EventArgs e)
+        {
+            if (labelDisplay0.Text.Length >0)
+                labelDisplay0.Text = 
+                    labelDisplay0.Text.Substring(0, labelDisplay0.Text.Length - 1);
+        }
+
+
+
+
+
+
+        private void buttonEqual_Clicked(object sender, EventArgs e)
+        {
+            string s1=labelDisplay0.Text;
+            
+          // var list1=new List<double>
+
+        }
+
+        private void buttonF_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonBracketLeft_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonBracketRight_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+
     }
 }
